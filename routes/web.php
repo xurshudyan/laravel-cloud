8efaf3e87ae3155064a47b0c8a9685331b88342a
Xurshudyan <v.khurshudyan@aico.swiss>
2025-02-24 22:11:04 +0400

    add test route

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+Route::get('/test', function () {
+    return 'test';
+});
+
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
